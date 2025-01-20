8675d8a11c90ea5295a654b0f0a019ad7eadbebe
Anish Pillai <anishpillaimca@gmail.com>
2025-01-20 17:39:29 -0500

    Removed Prisma for deployment

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,5 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
-import { PrismaClient } from "@prisma/client/extension"
-import { PrismaAdapter } from "@auth/prisma-adapter"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [GitHub],
